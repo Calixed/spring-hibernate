a737201dcd8f30e713d10c055e9bdeb13fda6caa
Calixed <arnievincent.nery@gmail.com>
2022-05-06 00:34:56 +0800

    Added FetchHQL class to demo you can query the lazy data

---
+++ src/demo/FetchJoin.java
@@ -0,0 +1,68 @@
+package demo;
+
+import entity.Course;
+import entity.Instructor;
+import entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class FetchJoin {
+
+    public static void main(String[] args) {
+        // heavy weight object ONLY CREATE ONCE
+        // used in generating sessions
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class) // to let hibernate know about our classes
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // the factory will be used to handled a session
+        Session session = factory.getCurrentSession();
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // option 2: Hibernate query with HQL
+
+            // get the instructor from db
+            int theId = 1;
+
+            Query<Instructor> query =
+                    session.createQuery("select i from Instructor i "
+                                    + "JOIN FETCH i.courses "
+                                    + "where i.id=:theInstructorId",
+                            Instructor.class);
+
+            // set parameter on query
+            query.setParameter("theInstructorId", theId);
+
+            // execute query and get instructor
+            Instructor tempInstructor = query.getSingleResult();
+
+            System.out.println("luv2code: Instructor: " + tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            // close the session
+            session.close();
+
+            System.out.println("\nluv2code: The session is now closed!\n");
+
+            // get courses for the instructor
+            System.out.println("luv2code: Courses: " + tempInstructor.getCourseList());
+
+            System.out.println("luv2code: Done!");
+        } finally {
+            factory.close();
+        }
+
+    }
+
+}
